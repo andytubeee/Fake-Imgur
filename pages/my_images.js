f0b5892229239172e81cdada5bdedfde52b2c29c
Andrew Yang <andy.tubeee@gmail.com>
2021-05-04 20:47:29 -0400

    Sign in option added in my_image route

--- pages/my_images.js
+++ pages/my_images.js
@@ -30,7 +30,7 @@ const MyImageCard = ({name, url, price, Isprivate}) => {
             if (result.isConfirmed) {
                 const newPrice = result.value
                 console.log(newPrice)
-                Swal.fire({title:'Coming Soon', text:'Feature under construction'})
+                Swal.fire({title: 'Coming Soon', text: 'Feature under construction'})
             }
         })
     }
@@ -39,9 +39,9 @@ const MyImageCard = ({name, url, price, Isprivate}) => {
         const uid = firebase.auth().currentUser.uid
         const userImgRef = db.collection("users").doc(uid)
 
-        userImgRef.get().then((doc)=>{
+        userImgRef.get().then((doc) => {
             doc.data().images.forEach(img => {
-                if (img.imgUrl === url && img.private === Isprivate){
+                if (img.imgUrl === url && img.private === Isprivate) {
                     userImgRef.update({
                         images: firebase.firestore.FieldValue.arrayRemove(img)
                     }).then(() => window.location.reload(false));
@@ -79,8 +79,10 @@ const MyImageCard = ({name, url, price, Isprivate}) => {
                 <img src={url} width={400} alt={name}/>
                 <div title={"Change"} className="d-flex justify-content-between align-items-center">
                     <div className="d-flex">
-                        <span style={{cursor: 'pointer', margin: '0 10px'}} className="badge bg-success" onClick={changePrice}>${price}</span>
-                        <span style={{cursor: 'pointer'}} className="badge bg-danger" onClick={deleteImage}>Delete</span>
+                        <span style={{cursor: 'pointer', margin: '0 10px'}} className="badge bg-success"
+                              onClick={changePrice}>${price}</span>
+                        <span style={{cursor: 'pointer'}} className="badge bg-danger"
+                              onClick={deleteImage}>Delete</span>
                     </div>
                     <div className="form-check form-switch align-self-end m-1">
                         <input
@@ -107,49 +109,75 @@ const my_images = () => {
 
     const db = firebase.firestore()
 
+    const [userSignedIn, setUserSignedIn] = useState(false)
     const [firestoreReady, setFirestoreReady] = useState(false)
     const [dName, setDName] = useState('')
     const [uid, setUID] = useState('')
     const [userImages, setUserimages] = useState([])
 
 
-    useEffect(()=>{
+    useEffect(() => {
         firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
+                setUserSignedIn(true)
                 setUID(user.uid);
                 setDName(user.displayName)
                 setFirestoreReady(true)
-            } else {
-                router.push("/");
+                if (firestoreReady) {
+                    const myImgRef = db.collection("users").doc(uid);
+                    myImgRef.get().then((doc) => {
+                        if (doc.exists) {
+                            if (doc.data().hasOwnProperty('images')) {
+                                doc.data().images.forEach(img => {
+                                    // console.log(img)
+                                    setUserimages(prev => [...prev, {
+                                        url: img.imgUrl,
+                                        name: img.imageName,
+                                        Isprivate: img.private,
+                                        price: img.price
+                                    }])
+                                })
+                            }
+                        }
+                    })
+                }
             }
         });
-        if (firestoreReady) {
-            const myImgRef = db.collection("users").doc(uid);
-            myImgRef.get().then((doc)=>{
-                if (doc.exists) {
-                    if(doc.data().hasOwnProperty('images')){
-                        doc.data().images.forEach(img => {
-                            // console.log(img)
-                            setUserimages(prev=>[...prev, {url: img.imgUrl, name: img.imageName, Isprivate: img.private, price: img.price}])
-                        })
-                    }
-                }
-            })
-        }
     }, [firestoreReady])
 
+    const loginWithGoogle = () => {
+        var provider = new firebase.auth.GoogleAuthProvider()
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                var user = result.user;
+                setUserSignedIn(true)
+            }).catch((error) => {
+            // Handle Errors here.
+            var errorMessage = error.message;
+            Swal.fire({title:"Error", text: errorMessage, icon:'error'})
+        });
+    }
 
     return (
         <div className="container d-flex flex-column">
-            <h1 style={{ textAlign: "center", marginTop: 20 }}>My Images</h1>
-            <button onClick={()=>{router.push('/home')}} className="btn btn-secondary mb-4" style={{width: '10%'}}>Home</button>
-            <p><b>User ID:</b> {uid}</p>
-            <p><b>Name:</b> {dName}</p>
+            <h1 style={{textAlign: "center", marginTop: 20}}>My Images</h1>
+            <button onClick={() => {
+                router.push('/home')
+            }} className="btn btn-secondary mb-4" style={{width: '10%'}}>Home
+            </button>
+            {!userSignedIn && <button className="btn btn-primary" onClick={loginWithGoogle}>Login with Google</button>}
+
+            {userSignedIn && (
+                <><p><b>User ID:</b> {uid}</p>
+                    <p><b>Name:</b> {dName}</p></>
+            )}
+
 
             {userImages.map(({name, url, price, Isprivate}) => (
                 <MyImageCard name={name} url={url} price={price} Isprivate={Isprivate}/>
             ))}
-            {userImages.length === 0 && <h1>You have no Images</h1>}
+            {userImages.length === 0 && userSignedIn && <h1>You have no Images</h1>}
 
         </div>
     );
